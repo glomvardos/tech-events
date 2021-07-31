3e6fca0ce238fefd638c10bc6b2eb5e20df8ace1
glomvardos <gerapes90@yahoo.gr>
2021-07-31 21:51:51 +0300

    Add event and input handlers

--- components/add-event-page/add-event.js
+++ components/add-event-page/add-event.js
@@ -1,12 +1,37 @@
+import { useRouter } from 'next/router'
 import styled from 'styled-components'
 import Form from '../ui/form'
 import StyledMain from '../ui/main'
 
 function AddEvent() {
+  const router = useRouter()
+  async function addEventHandler(event, inputs, fn) {
+    event.preventDefault()
+    const response = await fetch(`${process.env.API_URL}/events`, {
+      method: 'POST',
+      body: JSON.stringify(inputs),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+
+    // Clear Input Fields
+    fn({
+      title: '',
+      date: '',
+      time: '',
+      brief: '',
+      description: '',
+    })
+
+    router.replace(`/event/${data.slug}`)
+  }
+
   return (
     <StyledMain>
       <StyledTitle>Add Event</StyledTitle>
-      <Form />
+      <Form onSubmitHandler={addEventHandler} />
     </StyledMain>
   )
 }
